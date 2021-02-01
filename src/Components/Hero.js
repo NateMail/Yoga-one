df5c3d0a57f2d4bc15ec8b563dd94665861e14af
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-01 15:34:59 -0500

    Hero section grid

---
+++ src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="Hero__container">
+        <h1 className="Hero__container__text">Quotes Here</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
 
 const MainRouter = () => (
   <div>
+    <Hero />
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
